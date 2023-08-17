f95a84320168e4f6162b91f9856d287b59a6ee6a
YoucefRabia01 <rabiayoucef77@gmail.com>
2023-08-17 12:38:39 +0100

    lastcommit

---
+++ src/DS.java
@@ -0,0 +1,39 @@
+
+
+public class DS {
+
+    public static void main(String[] args) {
+       //Working with Java LinkedList
+
+
+        // LinkedList list = new LinkedList();
+
+        // list.addLast(5);
+        // list.addLast(4);
+        // list.addLast(3);
+        // list.addLast(2);
+        // list.addLast(1);
+        // System.out.println(list);
+        // System.out.println(list.indexOf(5));
+        // System.out.println(list.contains(5));
+        // System.out.println(list.size());
+        // list.remove(0);
+        // list.addLast(100);
+        // list.addFirst(99);
+        // System.out.println(list);
+
+        //Working with custom LinkedList    
+
+        LinkedList list = new LinkedList();
+
+        list.addLast(1);
+        list.addLast(1);
+        list.addLast(1);
+ 
+    
+    
+
+    }
+
+  
+}
---
+++ src/LinkedList.java
@@ -0,0 +1,93 @@
+public class LinkedList {
+    private Node first;
+    private Node last;
+
+    public LinkedList(){
+
+    }
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    public void addLast(int item) {
+        var node = new Node(item);
+ if(isEmpty()){
+  first = last = node;
+ }
+ else {
+    last.next = node;
+    last = node;
+}
+ }
+ public void addFirst(int item) {
+           var node = new Node(item);
+ if(isEmpty()){
+  first = last = node;
+ }
+ else {
+    first.next = node;
+    first = node;
+ }
+}
+ public int indexOf(int value){
+ int index = 0;
+ var current = first;
+ while( current != null){
+  if(current.value == value) return index;
+  else {
+    current = current.next;
+    index++;
+  }
+ } return -1;
+ }
+  public boolean contains(int value){
+ 
+ var current = first;
+ while( current != null){
+  if(current.value == value) return true;
+  else {
+    current = current.next;
+  }
+ } return false;
+ }
+
+ public void deleteFirst(){
+    if(isEmpty()){
+        throw new IllegalStateException();
+    }
+var second = first.next;
+first.next = null;
+first = second;
+ }
+ public void deleteLast(){
+        if(isEmpty()){
+        throw new IllegalStateException();
+    }
+    if(first == last){
+        first = last = null;
+    }
+
+  var previous = getPrevious(last);
+  last.next = null;
+  last = previous;
+
+ }
+   private Node getPrevious(Node node){
+   var current = first;
+    while (current != null) {
+        if(current.next == node) return current;current = current.next; //
+    }
+    
+     return null;
+   }
+  
+    private boolean isEmpty(){
+        return first == null;
+    }
+
+}
